20626d651e8b1d3274b2e10474e45901b2521474
Rafael Real <srerrereal@gmail.com>
2020-03-27 17:43:54 -0300

    commit on master

---
+++ app/script/app.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+		autoScrolling:false,
+		scrollHorizontally: true,
+		css3: true,
+		verticalCentered:true
+	});
+
+	//methods
+	$.fn.fullpage.setAllowScrolling(false);
+
+
+	/*$('.owl-carousel').owlCarousel({
+		loop:false,
+		margin:30,
+		nav:true,
+		navText:[
+			'<i class="fas fa-arrow-circle-right"></i>', '<i class="fas fa-arrow-circle-left"></i>'
+		],
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		},
+		center: true,
+		dotClass: 'owl-dots',
+		autoHeightClass: 'owl-height',
+		dragClass: 'owl-drag',
+	})
+*/
+	$('.carousel').carousel(
+		{
+
+		
+		
+		});
+	$('#my-carousel').carousel(
+
+	)
+
+
+});
+
